d25e0e5bafb313dda1b6476042abdc117f527364
NewLandTV <86148752+NewLandTV@users.noreply.github.com>
2022-10-10 14:14:07 +0900

    0.1 Commit

---
+++ Example2.cpp
@@ -0,0 +1,23 @@
+#include "한글"
+
+빈 초기화 괄호열기닫기 세미콜론
+
+정수32 진입점 괄호열기 정수32 매개변수개수 와 문자 포인터 포인터 매개변수목록 괄호닫기
+중괄호열기
+	초기화 괄호열기닫기 세미콜론
+	
+	반환 영 세미콜론
+중괄호닫기
+
+빈 초기화 괄호열기닫기
+중괄호열기
+	system 괄호열기 "title 한글" 괄호닫기 세미콜론
+	system 괄호열기 "mode con cols=60 lines=20" 괄호닫기 세미콜론
+
+	CONSOLE_CURSOR_INFO 콘솔커서정보 세미콜론
+
+	콘솔커서정보 멤버 bVisible 대입 영 세미콜론
+	콘솔커서정보 멤버 dwSize 대입 일 세미콜론
+
+	SetConsoleCursorInfo 괄호열기 GetStdHandle 괄호열기 STD_OUTPUT_HANDLE 괄호닫기 와 주소 콘솔커서정보 괄호닫기 세미콜론
+중괄호닫기
